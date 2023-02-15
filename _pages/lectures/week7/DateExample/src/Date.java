b2e70e2641e4807a43120d368c075683cbc802e2
Acacia Ackles <alackles@protonmail.com>
2023-02-15 10:47:29 -0600

    update date class

--- _pages/lectures/week7/DateExample/src/Date.java
+++ _pages/lectures/week7/DateExample/src/Date.java
@@ -3,17 +3,21 @@ public class Date {
     private int month;
     private int day;
     private int year; 
+    private String format;
+    public final String AUTHOR = "Acacia";
 
     public Date() {
         this.month = 1;
         this.day = 1;
         this.year = 1970;
+        this.format = "American";
     }
 
     public Date(int month, int day, int year) {
         this.month = month;
         this.day = day;
         this.year = year;
+        this.format = "American";
     }
 
     public int getMonth() {
@@ -25,18 +29,35 @@ public class Date {
     }
 
 
-    public void printDate() {
-        System.out.println(this.month + "/" + this.day + "/" + (this.year % 2000));
+    public static String getDefaultFormat() {
+        return "American";
     }
-    public static void main(String[] args) {
-        Date today = new Date();
-        today.day = 13;
-        today.month = 2;
-        today.year = 2023;
-        Date tomorrow = new Date(today.month, today.day + 1, today.year);
-        Date randomDay = new Date(7, 30, 1996);
-        System.out.println(today.year);
-        System.out.println("Date class");
+
+    public String getFormat() {
+        return this.format;
     }
+    
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String toString() {
+        if (this.format.equals("American")) {
+            return this.month + "/" + this.day + "/" + this.year;
+        } else {
+            return this.day + "/" + this.month + "/" + this.year;
+        }
+    }
+
+    public boolean equals(Date other) {
+        return this.month == other.month &&
+        this.day == other.day &&
+        this.year == other.year;
+    }
+
+    public boolean sameYear(Date other) {
+        return this.year == other.year;
+    }
+
 }
 
--- _pages/lectures/week7/DateExample/src/Report.java
+++ _pages/lectures/week7/DateExample/src/Report.java
@@ -1,12 +1,22 @@
 public class Report {
-    public static void main(String[] args) {
-        String weather = "cold";
+
+    public static void display(Date d, String weather) {
         System.out.print("Today's date is ");
-        Date today = new Date(2, 13, 2023);
-        today.printDate();
-        System.out.println(today.getMonth());
-        today.setMonth(7);
-        System.out.println(today.getMonth());
-        System.out.println("The weather is " + weather);
+        System.out.println(d);
+        System.out.print("Today's weather is ");
+        System.out.println(weather);
+    }
+    public static void main(String[] args) {
+        String weather = "rainy";
+        Date today = new Date(2, 15, 2023);
+        today.setFormat("European");
+        display(today, weather);
+        Date defaultDate = new Date();
+        Date unixZero = new Date(1, 20, 1970);
+        System.out.println(defaultDate);
+        System.out.println(unixZero);
+        System.out.println(defaultDate == unixZero);
+        System.out.println(defaultDate.equals(unixZero));
+        System.out.println(defaultDate.sameYear(unixZero));
     } 
 }
